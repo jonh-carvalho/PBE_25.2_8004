044631eb0a2987aa60cb1f68462e9c7e475621ca
jonh-carvalho <jonh.carvalho@professores.ibmec.edu.br>
2025-10-30 11:32:53 -0300

    update project

--- src/Streaming/content_app/views.py
+++ src/Streaming/content_app/views.py
@@ -3,6 +3,7 @@ from .models import Content
 from .serializers import ContentSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
